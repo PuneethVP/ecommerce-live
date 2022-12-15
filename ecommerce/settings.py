a10eec9ea844fd5c4483ebd7910babb49e75692d
PuneethVP <68001481+PuneethVP@users.noreply.github.com>
2022-12-15 12:15:42 +0530

    Update settings.py
    adding railway to allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'r3pc9=1^_gov2&u^lward6l^#oc@_9)antz+@5id-#e8_mub4m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-vp.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['ecommerce-vp.herokuapp.com','127.0.0.1','web-production-c0c0.up.railway.app']
 
 
 # Application definition
